89c7c812b7205b2bc469ef6ca6d9b19b01f255ea
EC2 Default User <ec2-user@ip-10-16-0-14.ec2.internal>
2022-11-15 20:58:58 +0000

    End of class

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+class ArticlesController < ApplicationController
+  def new
+  end
+  
+  def index
+    @articles = Article.all
+  end
+  
 end
